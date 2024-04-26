1594ebf4f1a71c080fb00907ff732ee6250aab68
Amanda Leticia Soranço <107198852+sorancoamanda26@users.noreply.github.com>
2024-04-26 10:49:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,39 @@ for (let i=0; i<botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }       
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-26T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-26T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-13T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function calculaTempo (tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " ;
+}
+
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
