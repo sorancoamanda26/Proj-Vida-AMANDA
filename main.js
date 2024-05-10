8fc9fef75b3ea088d5fd3b6729eb0c6402553eed
Amanda Leticia Soranço <107198852+sorancoamanda26@users.noreply.github.com>
2024-05-10 10:42:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,12 +35,19 @@ function calculaTempo (tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " ;
-}
-
+    if(tempoFinal > 0) {
+        return [dias, horas,minutos,segundos] ;
+    } else {
+        return[0,0,0,0];
+    }
+} 
+    
 function atualizaCronometro(){
     for(let i=0; i<contadores.length; i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 function comecaCronometro(){
